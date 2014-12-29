4016c0298cf73da855445db529460e05a5866fa1
黄勇 <huangyong@huangyongdeMBP.lan>
2014-12-29 21:02:59 +0800

    Initial Commit

---
+++ HYPasswordField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYPasswordField
+//
+//  Created by 黄勇 on 14/12/29.
+//  Copyright (c) 2014年 黄勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYPasswordField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYPasswordField
+//
+//  Created by 黄勇 on 14/12/29.
+//  Copyright (c) 2014年 黄勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
